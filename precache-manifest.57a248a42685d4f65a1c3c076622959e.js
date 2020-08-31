9175eca551b5f19ca04291cb7bf7f01100636434
RJ <blakemarveltone@gmail.com>
2020-08-31 19:30:47 +0800

    Updates

---
+++ precache-manifest.57a248a42685d4f65a1c3c076622959e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cfd9c0e9a90caaaa63efc3c28dd4fb",
+    "url": "/AJGuitar/index.html"
+  },
+  {
+    "revision": "5123b14bf4c9256505d5",
+    "url": "/AJGuitar/static/css/2.fc5c5e36.chunk.css"
+  },
+  {
+    "revision": "17a8d267fe6cadb58350",
+    "url": "/AJGuitar/static/css/main.fa66867f.chunk.css"
+  },
+  {
+    "revision": "5123b14bf4c9256505d5",
+    "url": "/AJGuitar/static/js/2.4ecbb6a7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/AJGuitar/static/js/2.4ecbb6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a8d267fe6cadb58350",
+    "url": "/AJGuitar/static/js/main.54898ed1.chunk.js"
+  },
+  {
+    "revision": "52b3fc6c41b63450a6f4",
+    "url": "/AJGuitar/static/js/runtime-main.0e065862.js"
+  },
+  {
+    "revision": "88db6409ff99818c721f269f3af26c4d",
+    "url": "/AJGuitar/static/media/carousel1.88db6409.jpg"
+  },
+  {
+    "revision": "d5d5a6075ed24e1ccd95c4340c538431",
+    "url": "/AJGuitar/static/media/carousel2.d5d5a607.jpg"
+  },
+  {
+    "revision": "4ae43c9c3c1c7c51f5ff93ead9874b30",
+    "url": "/AJGuitar/static/media/carousel3.4ae43c9c.jpg"
+  },
+  {
+    "revision": "eab849e3fb3bd79697716866f503eb45",
+    "url": "/AJGuitar/static/media/carousel4.eab849e3.jpg"
+  },
+  {
+    "revision": "200b607f0f61af43db2c37687529bc85",
+    "url": "/AJGuitar/static/media/carousel5.200b607f.jpg"
+  },
+  {
+    "revision": "4dcae6843a50c0649b6b4455ef9de54a",
+    "url": "/AJGuitar/static/media/carousel6.4dcae684.jpg"
+  },
+  {
+    "revision": "f8ca03faf65693893f5ead320e104f12",
+    "url": "/AJGuitar/static/media/logo.f8ca03fa.png"
+  },
+  {
+    "revision": "51d3087107d3635bbfb1882b402b4bf1",
+    "url": "/AJGuitar/static/media/money.51d30871.gif"
+  },
+  {
+    "revision": "7daec5f325f40dba4fb45b170b8afa93",
+    "url": "/AJGuitar/static/media/review 1.7daec5f3.png"
+  },
+  {
+    "revision": "242ff15681330770a9fafe437a11bc08",
+    "url": "/AJGuitar/static/media/review 2.242ff156.png"
+  },
+  {
+    "revision": "a9ec1ee0e58adc7daa27771c18c71dbc",
+    "url": "/AJGuitar/static/media/speed.a9ec1ee0.gif"
+  }
+]);
